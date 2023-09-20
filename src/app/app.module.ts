00768b266027f3e7b08a81809e1344e144f2078a
Romy Gaus <romy.p.gaus@gmail.com>
2023-09-20 08:44:28 +0200

    create LotLotR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuoteBoxComponent } from './quote-box/quote-box.component';
+import { GuessBoxComponent } from './guess-box/guess-box.component';
+import { AnswerBoxComponent } from './answer-box/answer-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuoteBoxComponent,
+    GuessBoxComponent,
+    AnswerBoxComponent
   ],
   imports: [
     BrowserModule
